5d9613ff139bd53984d4100e638535da65d56389
Rozhkov Ivan <vanroz333@gmail.com>
2023-10-08 21:35:27 +0700

    right

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://quotes.toscrape.com/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-quotes = soup.find_all('span', class_='text')
-authors = soup.find_all('small', class_='author')
-tags = soup.find_all('div', class_='tags')
+news = []
+for page in range(1, 4):
+    url = f'https://www.nstu.ru/news/?page={page}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    tags = soup.find_all('div', class_='bottomLine mb-2')
+    for tag in tags:
+        url = tag.select('a')[1]
+        title = url.text
+        link = url['href']
+        id = int(link.split('=')[-1])
+        news.append({'id': id, 'title': title})
 
-for i in range(0, len(quotes)):
-    print(quotes[i].text)
-    print('--' + authors[i].text)
-    tagsforquote = tags[i].find_all('a', class_='tag')
-    for tagforquote in tagsforquote:
-        print(tagforquote.text)
-    print('\n')
+print(news)
